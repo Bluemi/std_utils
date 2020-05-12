2829a94ae7ef49a7603b4273350f44bb7b5c46ac
Bluemi <bruno.schilling@protonmail.ch>
2020-05-12 21:33:40 +0200

    timer carries sound now

--- timer/install.sh
+++ timer/install.sh
@@ -3,7 +3,11 @@
 echo "installing \"timer\"..."
 
 mkdir -p $HOME/.local/bin
+mkdir -p $HOME/.local/etc/timer
 
+# install timer
+
+SOURCE_PATH="$PWD/timer"
 TARGET_PATH="$HOME/.local/bin/timer"
 
 if [ -e $TARGET_PATH ]; then
@@ -11,9 +15,26 @@ if [ -e $TARGET_PATH ]; then
 	exit 1
 fi
 
-echo "  creating symbolic link $PWD/timer -> $TARGET_PATH"
+echo "  creating symbolic link $SOURCE_PATH -> $TARGET_PATH"
 
-ln -s $PWD/timer $TARGET_PATH
+ln -s $SOURCE_PATH $TARGET_PATH
+
+
+# copy complete.oga
+
+SOURCE_PATH="$PWD/complete.oga"
+TARGET_PATH="$HOME/.local/etc/timer/complete.oga"
+
+if [ -e $TARGET_PATH ]; then
+	echo "  ERROR: complete.oga already exists at $TARGET_PATH"
+	exit 1
+fi
+
+echo "  creating symbolic link $SOURCE_PATH -> $TARGET_PATH"
+
+ln -s $SOURCE_PATH $TARGET_PATH
+
+# info
 
 if ! echo $PATH | grep "$HOME/\.local/bin" > /dev/null; then
 	echo "  to make \"timer\" work you should add $HOME/.local/bin to \$PATH!"
