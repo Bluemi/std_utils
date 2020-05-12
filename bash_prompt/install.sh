9e45bd75a580eaa3031136b202e208aa0a3bee14
Bluemi <bruno.schilling@protonmail.ch>
2020-05-12 19:07:00 +0200

    worked on install scripts

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -2,18 +2,29 @@
 
 # Installation script for installing shorter bash prompt
 
+echo "installing \"bash_prompt\"..."
+
+TARGET_PATH="$HOME/.local/bin/bash_prompt.py"
+
 mkdir -p $HOME/.local/bin
 
-ln -s $PWD/src/main.py $HOME/.local/bin/bash_prompt.py
-
-if ! echo $PATH | grep "$HOME/\.local/bin" > /dev/null; then
-	echo "to make shorter bash prompt work you should add \$HOME/.local/bin to \$PATH!"
+if [ -e "$TARGET_PATH" ]; then
+	echo "  ERROR: target $TARGET_PATH already exists"
+	exit 1
 fi
 
-echo "add the following lines into your bashrc to make bash prompt working"
+echo "  creating symbolic link $PWD/src/main.py -> $TARGET_PATH"
+
+ln -s $PWD/src/main.py $TARGET_PATH
+
+if ! echo $PATH | grep "$HOME/\.local/bin" > /dev/null; then
+	echo "  to make shorter bash prompt work you should add \$HOME/.local/bin to \$PATH!"
+fi
+
+echo "  add the following lines into your bashrc to make bash prompt working:"
 echo ""
 echo "function prompt_command {"
-echo "	CWD_TRIMMED="$(/home/alok/.local/bin/bash_prompt.py)""
+echo "    CWD_TRIMMED="$(/home/alok/.local/bin/bash_prompt.py)""
 echo "}"
 echo ""
 echo "PROMPT_COMMAND=prompt_command"
@@ -28,3 +39,5 @@ echo "PS1='\[\033[33m\]$CWD_TRIMMED\$\[\033[00m\] '"
 # 
 # PROMPT_COMMAND=prompt_command
 # PS1='\[\033[33m\]$CWD_TRIMMED\$\[\033[00m\] '
+
+echo "finished \"bash_prompt\" installation"
--- timer/install.sh
+++ timer/install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+echo "installing \"timer\"..."
+
 mkdir -p $HOME/.local/bin
 
-ln -s $PWD/timer $HOME/.local/bin/timer
+TARGET_PATH="$HOME/.local/bin/timer"
+
+if [ -e $TARGET_PATH ]; then
+	echo "  ERROR: timer already exists at $TARGET_PATH"
+	exit 1
+fi
+
+echo "  creating symbolic link $PWD/timer -> $TARGET_PATH"
+
+ln -s $PWD/timer $TARGET_PATH
 
 if ! echo $PATH | grep "$HOME/\.local/bin" > /dev/null; then
-	echo "to make timer work you should add \$HOME/.local/bin to \$PATH!"
+	echo "  to make \"timer\" work you should add $HOME/.local/bin to \$PATH!"
 fi
+
+echo "finished \"timer\" installation"
