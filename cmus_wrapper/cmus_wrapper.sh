61e60141c0684556fa66714271f0577790590d03
Bluemi <bruno.schilling@protonmail.ch>
2021-07-23 12:25:12 +0200

    screen -> tmux; cmus -> musicus

--- cmus_wrapper/cmus_wrapper.sh
+++ cmus_wrapper/cmus_wrapper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-screen -D -R -S cmus /home/alok/.local/bin/mount_cmus.sh
+# screen -D -R -S cmus /home/alok/.local/bin/mount_cmus.sh
+tmux -u -2 new-session -A -D -s cmus /home/alok/.local/bin/mount_cmus.sh
--- cmus_wrapper/mount_cmus.sh
+++ cmus_wrapper/mount_cmus.sh
@@ -9,8 +9,8 @@ sshfs alok@Strubbel:/Musik/MusicBruno17 "$MUSIC_PATH"
 result=$?
 
 if [ "$result" -eq "0" ]; then
-	# start cmus
-	/home/alok/.local/bin/cmus
+	# start musicus
+	RUST_BACKTRACE=full /home/alok/.local/bin/musicus 2> /home/alok/.local/share/musicus.log
 else
 	echo "failed to mount $MUSIC_PATH"
 	sleep 3
