572521b9b638166fa7e5a242422526a396e62522
Bluemi <bruno.schilling@protonmail.ch>
2022-01-24 16:11:17 +0100

    add sh_new

---
+++ sh_new/sh_new.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+filename=
+execute_permissions=
+
+while true; do
+	if [ -z "$1" ]; then
+		break
+	fi
+
+	case "$1" in
+		-x|--execute-permissions)
+			execute_permissions=1
+			;;
+		*)
+			if [ -n "$filename" ]; then
+				echo "To many positional arguments: $1"
+				exit 1
+			fi
+			filename="$1"
+			;;
+	esac
+	shift
+done
+
+if [ -z "$filename" ]; then
+	filename="run.sh"
+fi
+
+if [ -f "$filename" ]; then
+	echo "ERROR: file $filename already exists"
+	exit 1
+fi
+
+echo "#!/bin/bash" > "$filename"
+echo "" >> "$filename"
+echo "" >> "$filename"
+
+if [ -n "$execute_permissions" ]; then
+	chmod u+x "$filename"
+fi
+
+vi "$filename"
