7fd863a677fd35be44b5c380534ef1d1be7c6c4f
Bluemi <bruno.schilling@protonmail.ch>
2022-07-17 14:22:58 +0200

    minor changes

--- backup/backup_now.sh
+++ backup/backup_now.sh
@@ -2,7 +2,7 @@
 
 EXCLUDE_FILE="$HOME/.local/etc/backup_now/excludes.txt"
 
-if [ -d "$HOME/Backup/BlackBoxBackup" ]; then
+if [ -d "$HOME/Backup" ]; then
 	mount_strubbel backup  # unmount
 fi
 rsync -ah --delete --info=progress2 --exclude-from "$EXCLUDE_FILE" "$HOME/" "strubbel:/volume3/Backup/BlackBoxBackup/"
