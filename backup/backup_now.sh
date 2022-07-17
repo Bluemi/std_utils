4fd53248cbecd0d961f3cfd4e3b928d144bb4e76
Bluemi <bruno.schilling@protonmail.ch>
2022-07-17 14:22:40 +0200

    improved backup script

--- backup/backup_now.sh
+++ backup/backup_now.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 EXCLUDE_FILE="$HOME/.local/etc/backup_now/excludes.txt"
-TARGET_DIR="./Backup/BlackBoxBackup"
 
-if [ ! -d "$TARGET_DIR" ]; then
-	mount_strubbel backup
-fi
-rsync -ah --delete --info=progress2 --exclude-from "$EXCLUDE_FILE" "$HOME/" "$TARGET_DIR"
-if [ -d "$TARGET_DIR" ]; then
+if [ -d "$HOME/Backup/BlackBoxBackup" ]; then
 	mount_strubbel backup  # unmount
 fi
+rsync -ah --delete --info=progress2 --exclude-from "$EXCLUDE_FILE" "$HOME/" "strubbel:/volume3/Backup/BlackBoxBackup/"
