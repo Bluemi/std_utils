184e78711083bcdb75bc513c0b70edfcc14956a5
Bluemi <bruno.schilling@protonmail.ch>
2022-07-17 15:17:41 +0200

    minor changes

--- backup/backup_now.sh
+++ backup/backup_now.sh
@@ -5,4 +5,18 @@ EXCLUDE_FILE="$HOME/.local/etc/backup_now/excludes.txt"
 if [ -d "$HOME/Backup" ]; then
 	mount_strubbel backup  # unmount
 fi
-rsync -ah --delete --info=progress2 --exclude-from "$EXCLUDE_FILE" "$HOME/" "strubbel:/volume3/Backup/BlackBoxBackup/"
+
+case $HOSTNAME in
+	blackbox)
+		dir="BlackBoxBackup"
+		;;
+	lightbox)
+		dir="LightBoxBackup"
+		;;
+	*)
+		echo "ERROR: unknown host"
+		exit 1
+		;;
+esac
+
+rsync -ah --delete --info=progress2 --exclude-from "$EXCLUDE_FILE" "$HOME/" "strubbel:/volume3/Backup/$dir/"
