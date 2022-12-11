f8d4e782e8015d8b80348d38ceea1224155c53d2
Bluemi <bruno.schilling@protonmail.ch>
2022-12-11 12:57:04 +0100

    add file remover

---
+++ file_remover/file_remover.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+directory="."
+
+if [ -n "$1" ]; then
+	directory="$1"
+fi
+
+for filename in *; do
+	case "${filename,,}" in
+		*.mp4|*.mp3|*.mov|*.webm)
+			vlc "$filename"
+			;;
+		*.jpg|*.png)
+			gwenview "$filename"
+			;;
+		*)
+			echo "no fitting program found for $filename"
+			;;
+	esac
+
+	echo -n "remove file (y[es], n[o], q[uit]): "
+	read userinput
+	case "$userinput" in
+		y|yes)
+			rm -v "$filename"
+			;;
+		n|no)
+			;;
+		q|quit)
+			break
+			;;
+		*)
+			echo "invalid userinput: $userinput"
+			;;
+	esac
+done
