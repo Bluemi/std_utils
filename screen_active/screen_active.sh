b06924467e6be826c2390bf9271d7af4759e4eba
Bluemi <bruno.schilling@protonmail.ch>
2023-05-30 14:21:22 +0200

    minor improvements

--- screen_active/screen_active.sh
+++ screen_active/screen_active.sh
@@ -2,7 +2,6 @@
 
 while true; do
 	sleep $((60*5))
-	xdotool mousemove_relative 10 0
-	sleep $((60*5))
-	xdotool mousemove_relative -- -10 0
+	xdotool mousemove_relative 1 0
+	xdotool mousemove_relative -- -1 0
 done
