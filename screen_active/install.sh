a5500ca7b0a78cdefeae88810454a972e0cd646e
Bluemi <bruno.schilling@protonmail.ch>
2023-10-06 09:32:20 +0200

    improved install.sh for screen-active

--- screen_active/install.sh
+++ screen_active/install.sh
@@ -13,8 +13,22 @@ echo -e "installing ${GREEN}$SCRIPT_NAME${NC}"
 
 mkdir -p $HOME/.local/bin
 
+# Add screen-active
 SOURCE_PATH="$PWD/$SCRIPT_NAME.sh"
 TARGET_PATH="$HOME/.local/bin/screen-active"
+
+if [ -e $TARGET_PATH ]; then
+	echo -e "  ${YELLOW}SKIP:${NC} $SCRIPT_NAME already exists at ${CYAN}$TARGET_PATH${NC}"
+	exit 1
+fi
+
+echo -e "  creating symbolic link ${CYAN}${SOURCE_PATH}${NC} -> ${CYAN}${TARGET_PATH}${NC}"
+
+ln -s $SOURCE_PATH $TARGET_PATH
+
+# add screen-active-impl
+SOURCE_PATH="$PWD/$SCRIPT_NAME-impl.sh"
+TARGET_PATH="$HOME/.local/bin/screen-active-impl"
 
 if [ -e $TARGET_PATH ]; then
 	echo -e "  ${YELLOW}SKIP:${NC} $SCRIPT_NAME already exists at ${CYAN}$TARGET_PATH${NC}"
