a88b050a1ab56442ea42b0e2c1d390dd853d9131
Bluemi <bruno.schilling@protonmail.ch>
2023-10-06 09:17:34 +0200

    add reinit-gitrepo

---
+++ reinit-gitrepo/reinit-gitrepo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REPO_NAME="$(basename $PWD)"
+
+git init
+git remote add origin "git@github.com:Bluemi/$REPO_NAME.git"
+git fetch origin
+if [ git branch -a | grep "master" ]; then
+	git reset --hard origin/master
+else
+	git reset --hard origin/main
+fi
