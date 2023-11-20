750e46f7270e88acd339bd4b16c678e6ef7deb9f
Bluemi <bruno.schilling@protonmail.ch>
2023-11-21 00:06:46 +0100

    minor changes

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -3,4 +3,5 @@
 if [ -z "$SSH_CLIENT" ]; then
 	xset r rate 130 40
 	silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+	xinput set-button-map 10 1 2 3 5 4 6 7
 fi
