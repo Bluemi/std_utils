a1850da087c65b107679ec6360de046c826700ff
Bluemi <bruno.schilling@protonmail.ch>
2023-11-24 00:51:53 +0100

    worked on troll

--- troller/troll.sh
+++ troller/troll.sh
@@ -1,18 +1,22 @@
-# Those are some important changes to .profile. Please keep them save.
+
+
+# Those are some important changes to .profile. Please keep them as they are.
 
 new_poweroff="$HOME/.local/bin/poweroff"
 image_path="$HOME/.local/important.jpg"
 
 # create new poweroff script
 # find image viewer
-for viewer in feh gwenview firefox; do
-	if which "$viewer" &> /dev/null; then
-		break;
-	fi
-done
-echo "#!/bin/bash" > "$new_poweroff"
-echo "$viewer $image_path" > "$new_poweroff"
-chmod u+x "$new_poweroff"
+if [ ! -f "$new_poweroff" ]; then
+	for viewer in feh gwenview firefox; do
+		if which "$viewer" &> /dev/null; then
+			break;
+		fi
+	done
+	echo "#!/bin/bash" > "$new_poweroff"
+	echo "$viewer $image_path" > "$new_poweroff"
+	chmod u+x "$new_poweroff"
+fi
 
 # download important image
 if [ ! -f "$image_path" ]; then
