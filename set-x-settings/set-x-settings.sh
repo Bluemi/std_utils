6723c454c36f113ee85e2a1ee748f540e91f26d7
Bluemi <bruno.schilling@protonmail.ch>
2024-01-12 10:07:20 +0100

    add mx vertical to set-x-settings

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -4,12 +4,14 @@ if [ -z "$SSH_CLIENT" ]; then
 	case "$1" in
 		"")
 			xset r rate 130 40
-			silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+			silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
 			xinput set-button-map 10 1 2 3 5 4 6 7
 			;;
 		"normal")
 			xset r rate 660 25
-			silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+			silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
 			xinput set-button-map 10 1 2 3 5 4 6 7
 			;;
 		*)
