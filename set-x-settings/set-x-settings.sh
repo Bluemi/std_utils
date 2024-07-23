fd57afb940ce3f2a9b3562ad8ece023276966f3c
Bluemi <bruno.schilling@protonmail.ch>
2024-07-23 21:33:28 +0200

    minor changes

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -16,6 +16,13 @@ if [ -z "$SSH_CLIENT" ]; then
 			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
 			xinput set-button-map 10 1 2 3 5 4 6 7
 			;;
+		"slow")
+			xset r rate 1660 10
+			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
+			# silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
+			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
+			xinput set-button-map 10 1 2 3 5 4 6 7
+			;;
 		*)
 			echo "unknown option \"$1\""
 			;;
