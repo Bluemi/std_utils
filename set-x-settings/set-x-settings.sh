cfa0e25633b03accfb1562daf77094002c9c8298
Bluemi <bruno.schilling@protonmail.ch>
2024-07-25 21:43:26 +0200

    minor changes

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+function set_xinput_props {
+  case "$HOSTNAME" in
+    lightbox)
+			xinput set-prop 10 329 1  # inverse trackpad scroll direction
+			xinput set-button-map 10 1 2 3 5 4 6 7
+			;;
+		blackbox)
+			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
+			xinput set-button-map 10 1 2 3 5 4 6 7
+			;;
+	esac
+}
+
 if [ -z "$SSH_CLIENT" ]; then
 	case "$1" in
 		"")
 			xset r rate 130 40
-			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
-			# silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
-			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
-			xinput set-button-map 10 1 2 3 5 4 6 7
+			set_xinput_props
 			;;
 		"normal")
 			xset r rate 660 25
-			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
-			# silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
-			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
-			xinput set-button-map 10 1 2 3 5 4 6 7
+			set_xinput_props
 			;;
 		"slow")
 			xset r rate 1660 10
-			# silent xinput --set-prop "Microsoft Microsoft® Classic IntelliMouse®" "libinput Natural Scrolling Enabled" 1
-			# silent xinput --set-prop "pointer:Logitech MX Vertical" "libinput Natural Scrolling Enabled" 1
-			silent xinput --set-prop "pointer:Logitech MX Vertical Advanced Ergonomic Mouse" "libinput Natural Scrolling Enabled" 1
-			xinput set-button-map 10 1 2 3 5 4 6 7
+			set_xinput_props
 			;;
 		*)
 			echo "unknown option \"$1\""
