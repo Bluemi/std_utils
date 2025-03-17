874fd13d00b9d4b5fcfc745a3d7721ed0caca2da
Bluemi <bruno.schilling@protonmail.ch>
2025-03-17 10:04:59 +0100

    use names for natural scrolling on lightbox

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -3,7 +3,7 @@
 function set_xinput_props {
   case "$HOSTNAME" in
     lightbox)
-			xinput set-prop 10 331 1  # inverse trackpad scroll direction
+			xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
 			xinput set-button-map 10 1 2 3 5 4 6 7
 			;;
 		blackbox)
