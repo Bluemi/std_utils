2feb7f2b840ac2dc584cd20b717c3bab02100fde
Bluemi <bruno.schilling@protonmail.ch>
2025-10-27 12:57:24 +0100

    support more mouse for natural scrolling

--- set-x-settings/set-x-settings.sh
+++ set-x-settings/set-x-settings.sh
@@ -7,12 +7,12 @@ function set_xinput_props {
 			xinput set-button-map 10 1 2 3 5 4 6 7
 			;;
 		blackbox)
-			mouse_id=$(xinput list | grep 'Logitech MX Vertical Advanced Ergonomic Mouse' | grep -v keyboard | grep -o 'id=[0-9]*' | cut -d= -f2 | head -n1)
+			mouse_id=$(xinput list | grep -E 'Logitech MX Vertical Advanced Ergonomic Mouse|Logitech USB Receiver Mouse' | grep -v keyboard | grep -o 'id=[0-9]*' | cut -d= -f2 | head -n1)
 			natural_scrolling_id="$(xinput list-props "$mouse_id" | grep -i 'natural scrolling' | grep -v 'Default' | grep -oP '\(\K[0-9]+')"
 
 			# inverse scroll direction for mouse
 			xinput set-prop "$mouse_id" "$natural_scrolling_id" 1
-			xinput set-button-map 10 1 2 3 5 4 6 7
+			xinput set-button-map 10 1 2 3 5 4 6 7  2>/dev/null
 			;;
 	esac
 }
