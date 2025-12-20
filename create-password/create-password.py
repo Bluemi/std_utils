c5aa65127ca1bb8f2eaa0b6d59c516d1462896fd
Bluemi <bruno.schilling@protonmail.ch>
2025-12-20 10:22:13 +0100

    improve password security

--- create-password/create-password.py
+++ create-password/create-password.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 import string
-import random
+import secrets
 
 
 def main():
-    all_chars = string.ascii_letters + string.digits  #  + ".-_:%/&!$=+*#'\",;<>"
-    pw = random.choices(all_chars, k=16)
+    chars = string.ascii_letters + string.digits  #  + ".-_:%/&!$=+*#'\",;<>"
+    pw = create_pw(chars, k=16)
     print(''.join(pw))
+
+
+def create_pw(chars, k=16):
+    pw = [secrets.choice(chars) for _ in range(k)]
+    return ''.join(pw)
 
 
 if __name__ == '__main__':
