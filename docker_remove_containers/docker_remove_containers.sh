69676e5d6599c23fa7969a87100e53446e7552d0
Bluemi <bruno.schilling@protonmail.ch>
2026-01-06 17:03:47 +0100

    minor changes

--- docker_remove_containers/docker_remove_containers.sh
+++ docker_remove_containers/docker_remove_containers.sh
@@ -2,10 +2,12 @@
 
 CONTAINERS=$(docker ps -a -q)
 if [ -n "$CONTAINERS" ]; then
+	echo "stopping containers:"
 	docker stop -t 1 "$CONTAINERS"
 fi
 
 CONTAINERS=$(docker ps -a -q)
 if [ -n "$CONTAINERS" ]; then
+	echo "removing containers:"
 	docker rm $(docker ps -a -q)
 fi
